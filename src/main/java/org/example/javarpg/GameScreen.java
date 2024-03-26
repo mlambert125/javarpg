ac16c5716a93611f6844687ae8dfbe494eb5cb60
Michael Lambert <mlambert125@live.com>
2024-03-25 22:31:34 -0400

    Smooth scrolling and walkability

--- src/main/java/org/example/javarpg/GameScreen.java
+++ src/main/java/org/example/javarpg/GameScreen.java
@@ -5,52 +5,51 @@ import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 
 public class GameScreen extends Screen {
-    private boolean walking = false;
+    private static final float SMOOTH_STEP = 0.125f;
     private Map map = new Map();
-    private int characterX = 15;
-    private int characterY = 15;
-
-    private long lastPositionUpdateFrame = 0;
+    private float characterX = 15;
+    private float characterY = 15;
 
     @Override
     public void update(Application application, KeyboardState keyboardState, GameState gameState, long currentFrame) {
         if (keyboardState.isKeyEscape()) {
             application.setCurrentScreen(new TitleScreen());
         }
-        if (keyboardState.isKeyDown()) {
-            gameState.getSprite().setDirection(CharacterSprite.DIRECTION_DOWN);
-            walking = true;
 
-            if (currentFrame - lastPositionUpdateFrame > 10) {
-                lastPositionUpdateFrame = currentFrame;
-                characterY++;
-            }
-        } else if (keyboardState.isKeyLeft()) {
-            gameState.getSprite().setDirection(CharacterSprite.DIRECTION_LEFT);
-            walking = true;
-
-            if (currentFrame - lastPositionUpdateFrame > 10) {
-                lastPositionUpdateFrame = currentFrame;
-                characterX--;
-            }
-        } else if (keyboardState.isKeyRight()) {
-            gameState.getSprite().setDirection(CharacterSprite.DIRECTION_RIGHT);
-            walking = true;
-
-            if (currentFrame - lastPositionUpdateFrame > 10) {
-                lastPositionUpdateFrame = currentFrame;
-                characterX++;
-            }
-        } else if (keyboardState.isKeyUp()) {
-            gameState.getSprite().setDirection(CharacterSprite.DIRECTION_UP);
-            walking = true;
-
-            if (currentFrame - lastPositionUpdateFrame > 10) {
-                lastPositionUpdateFrame = currentFrame;
-                characterY--;
+        if (characterX - (int) characterX == 0 && characterY - (int) characterY == 0) {
+            // We are on a specific tile and should read keyboard input to maybe move
+            if (keyboardState.isKeyDown()) {
+                gameState.getSprite().setDirection(CharacterSprite.DIRECTION_DOWN);
+                if (map.isTileWalkable((int)characterX, (int)characterY + 1)) {
+                    characterY += SMOOTH_STEP;
+                }
+            } else if (keyboardState.isKeyLeft()) {
+                gameState.getSprite().setDirection(CharacterSprite.DIRECTION_LEFT);
+                if (map.isTileWalkable((int)characterX - 1, (int)characterY)) {
+                    characterX -= SMOOTH_STEP;
+                }
+            } else if (keyboardState.isKeyRight()) {
+                gameState.getSprite().setDirection(CharacterSprite.DIRECTION_RIGHT);
+                if (map.isTileWalkable((int)characterX + 1, (int)characterY)) {
+                    characterX += SMOOTH_STEP;
+                }
+            } else if (keyboardState.isKeyUp() ) {
+                gameState.getSprite().setDirection(CharacterSprite.DIRECTION_UP);
+                if (map.isTileWalkable((int)characterX, (int)characterY - 1)) {
+                    characterY -= SMOOTH_STEP;
+                }
             }
         } else {
-            walking = false;
+            // We are in the middle of a tile and should continue moving
+            if (gameState.getSprite().getDirection() == CharacterSprite.DIRECTION_DOWN) {
+                characterY += SMOOTH_STEP;
+            } else if (gameState.getSprite().getDirection() == CharacterSprite.DIRECTION_LEFT) {
+                characterX -= SMOOTH_STEP;
+            } else if (gameState.getSprite().getDirection() == CharacterSprite.DIRECTION_RIGHT) {
+                characterX += SMOOTH_STEP;
+            } else if (gameState.getSprite().getDirection() == CharacterSprite.DIRECTION_UP) {
+                characterY -= SMOOTH_STEP;
+            }
         }
     }
 
@@ -59,11 +58,22 @@ public class GameScreen extends Screen {
         ctx.setFill(Color.DARKGREEN);
         ctx.fillRect(0, 0, 704, 704);
 
-
         map.draw(ctx, characterX, characterY);
 
+        int step = 0;
+        float stepDiff = (characterX - (int)characterX);
 
-        int step = walking ? (int) (currentFrame / 10 % 4) : 1;
+        if (stepDiff == 0) {
+            stepDiff = (characterY - (int)characterY);
+        }
+
+        if (stepDiff > 0 && stepDiff <= 0.25) {
+            step = 1;
+        } else if (stepDiff > 0.25 && stepDiff <= 0.5) {
+            step = 2;
+        } else if (stepDiff > 0.5 && stepDiff <= 0.75) {
+            step = 3;
+        }
 
         CharacterSprite sprite = gameState.getSprite();
         sprite.draw(step, ctx, 320, 320);
--- src/main/java/org/example/javarpg/Map.java
+++ src/main/java/org/example/javarpg/Map.java
@@ -75,15 +75,18 @@ public class Map {
         imageTiles = new Image("file:src/main/resources/tiles.png");
     }
 
-    public void draw(GraphicsContext ctx, int characterX, int characterY) {
-        int mapOffsetX = characterX - 5;
-        int mapOffsetY = characterY - 5;
+    public void draw(GraphicsContext ctx, float characterX, float characterY) {
+        int mapOffsetX = (int)characterX - 5;
+        int mapOffsetY = (int)characterY - 5;
+
+        int subOffsetX = (int)((characterX - (int)characterX) * 64);
+        int subOffsetY = (int)((characterY - (int)characterY) * 64);
 
         int screenX = 0;
         int screenY = 0;
 
-        for (int x = mapOffsetX; x < mapOffsetX + 11; x++) {
-            for (int y = mapOffsetY; y < mapOffsetY + 11; y++) {
+        for (int x = mapOffsetX; x <= mapOffsetX + 11; x++) {
+            for (int y = mapOffsetY; y <= mapOffsetY + 11; y++) {
                 if (x < 0 || y < 0 || x >= tiles[0].length || y >= tiles.length) {
                     screenY += 64;
                     continue;
@@ -93,11 +96,19 @@ public class Map {
                 int sheetOffsetX = (tile % 4) * 64;
                 int sheetOffsetY = (tile / 4) * 64;
 
-                ctx.drawImage(imageTiles, sheetOffsetX, sheetOffsetY, 64, 64, screenX, screenY, 64, 64);
+                ctx.drawImage(imageTiles, sheetOffsetX, sheetOffsetY, 64, 64, screenX - subOffsetX, screenY - subOffsetY, 64, 64);
                 screenY += 64;
             }
             screenX += 64;
             screenY = 0;
         }
+    }
+
+    public boolean isTileWalkable(int x, int y) {
+        if (x < 0 || y < 0 || x >= tiles[0].length || y >= tiles.length) {
+            return false;
+        }
+        int tile = tiles[y][x];
+        return tile != TILE_TREE && tile != TILE_ROCK;
     }
 }
